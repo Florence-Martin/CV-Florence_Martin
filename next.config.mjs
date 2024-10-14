54a57a4520a32c4ba84c6fcb51608124a8ed85b5
Florence Martin <florence.martin469@gmail.com>
2024-10-14 11:48:55 +0200

    fix: vercel file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true, // Activer l'API appDir si vous utilisez le dossier 'app/'
+  },
+};
 
 export default nextConfig;
